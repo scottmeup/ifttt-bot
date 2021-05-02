e2a559e713a859bed39ab9fb65768866645ab2a8
Andrew Scott <scottmeup@users.noreply.github.com>
2021-05-02 14:14:12 +1000

    Update iftttbot.py
    Add date + time to output

--- iftttbot.py
+++ iftttbot.py
@@ -4,6 +4,7 @@ from time import sleep
 import requests, threading,glob,sys
 import bs4 as bs
 from selenium.webdriver.common.action_chains import ActionChains
+from datetime import datetime
 
 
 if __name__ == '__main__':
@@ -17,43 +18,47 @@ if __name__ == '__main__':
     appletID = sys.argv[3]
 
     print("Checking if the email and password are correct")
-    options = Options()
-    # options.add_argument("--incognito")
-    # options.add_argument("--headless")
-    # options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
-    
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(executable_path=r"/usr/local/bin/chromedriver",options=options)    
     '''
     make checking window invisible
     '''
-    #browser.set_window_position(5000, 5000) 
+    driver.set_window_position(5000, 5000) 
     
-    browser.get("https://ifttt.com/login?wp_=1")
+    driver.get("https://ifttt.com/login?wp_=1")
     sleep(1)
-    browser.find_element_by_id("user_username").send_keys(email)
+    driver.find_element_by_id("user_username").send_keys(email)
     sleep(1)
-    browser.find_element_by_id("user_password").send_keys(password)
+    driver.find_element_by_id("user_password").send_keys(password)
     sleep(1)
-    browser.find_element_by_name("commit").click()
+    driver.find_element_by_name("commit").click()
     sleep(1)
 
     expected_url = "https://ifttt.com/home"
-    login_success = expected_url == browser.current_url
-    print("expected_url = " + expected_url + ", browser.current_url = " + browser.current_url)
+    login_success = expected_url == driver.current_url
+    print("expected_url = " + expected_url + ", .current_url = " + driver.current_url)
     print("successfull login = " + str(login_success))
     if not(login_success):
-        browser.close()
+        driver.close()
         print("Username / Password combination not valid")
         print("Username: " + email + " Password: " + password)
         exit()
 
     url = ("https://ifttt.com/applets/" + appletID)
-    browser.get(url)
+    driver.get(url)
     sleep(2)
 
-    button = browser.find_element_by_link_text('Check now')
-    browser.implicitly_wait(10)
-    ActionChains(browser).move_to_element(button).click(button).perform()
+    button = driver.find_element_by_link_text('Check now')
+    driver.implicitly_wait(10)
+    ActionChains(driver).move_to_element(button).click(button).perform()
     
-    sleep(4)
-    browser.close()
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    print("Check completed at ", dt_string)
+
+    sleep(5)
+    driver.close()
